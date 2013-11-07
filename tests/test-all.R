dbd53745a846af686c73beb6a27f9d29b0f501d0
jyypma <jyypma@users.noreply.github.com>
2013-11-07 21:49:03 +0000

    Temporarily disable testthat tests.
    git-svn-id: svn+ssh://scm.r-forge.r-project.org/svnroot/nloptr/pkg@82 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- tests/test-all.R
+++ tests/test-all.R
@@ -1,5 +1,6 @@
 if(require("testthat", quietly = TRUE)) {
-    test_check("nloptr")
+    #test_check("nloptr")
+    #test_package("nloptr")
 } else {
     print( "package 'testthat' not available, cannot run unit tests" )
 }
