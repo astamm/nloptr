21bfba850444e1d69f5ccfb09853c5a2fef855e9
WillForan <willforan@gmail.com>
2017-06-12 12:49:18 -0400

    revert PkgFlags

--- R/PkgFlags.R
+++ R/PkgFlags.R
@@ -3,7 +3,7 @@ CFlags <- function( print = TRUE ) {
     if (.Platform$OS.type=="windows") {
         flags <- '-I"$(NLOPT_HOME)$(R_ARCH)/include"' 
     } else {
-        flags <- '  -I/home/foranw/src/world/R/nloptr/nlopt-2.4.2/include'
+        flags <- '@PKG_CFLAGS@'
     }
     if ( print ) {
         cat( flags )
@@ -17,7 +17,7 @@ LdFlags <- function( print = TRUE ) {
     if (.Platform$OS.type=="windows") {
         flags <- '-L"$(NLOPT_HOME)$(R_ARCH)/lib" -lnlopt_cxx'
     } else {
-        flags <- '  -lm /home/foranw/src/world/R/nloptr/nlopt-2.4.2/lib/libnlopt_cxx.a'
+        flags <- '@PKG_CFLAGS@'
     }
     if ( print ) {
         cat( flags )
