e76f189e62edbc201c8728e6d0299287f294652e
jyypma <jyypma@users.noreply.github.com>
2018-04-17 21:30:54 +0200

    Changed convergence tolerance in test
    Test HS 023 failed on Solaris due to optimal solution being outside
    tolerance.
    
    ```
    Failure: Test HS023. (@test-hs023.R#90)
    `x` not equal to `expected`.
    2/2 mismatches (average diff: 3.61e-07)
    [1] 1 - 1 == -5.15e-07
    [2] 1 - 1 == -2.08e-07
    ```

--- tests/testthat/test-hs023.R
+++ tests/testthat/test-hs023.R
@@ -23,7 +23,7 @@
 #   1 - x1^2 - x2^2 <= 0
 #   9 - 9*x1^2 - x2^2 <= 0
 #   x2 - x1^2 <= 0
-#   x1 - x2^2 <= 0 
+#   x1 - x2^2 <= 0
 #
 # the initial value is
 # x0 = (3, 1)
@@ -39,12 +39,12 @@ test_that( "Test HS023.", {
     #
     # f(x) = x1^2 + x2^2
     #
-    eval_f <- function( x ) { 
+    eval_f <- function( x ) {
         return( list( "objective" = x[1]^2 + x[2]^2,
                       "gradient" = c( 2*x[1],
-                                      2*x[2] ) ) ) 
+                                      2*x[2] ) ) )
     }
-    
+
     # Inequality constraints.
     eval_g_ineq <- function( x ) {
         constr <- c( 1 - x[1] - x[2],
@@ -52,45 +52,45 @@ test_that( "Test HS023.", {
                      9 - 9*x[1]^2 - x[2]^2,
                      x[2] - x[1]^2,
                      x[1] - x[2]^2 )
-                     
+
         grad   <- rbind( c( -1, -1 ),
                          c( -2*x[1], -2*x[2] ),
                          c( -18*x[1], -2*x[2] ),
                          c( -2*x[1], 1 ),
                          c( 1, -2*x[2] ) )
-                         
+
         return( list( "constraints"=constr, "jacobian"=grad ) )
     }
-    
+
     # Initial values.
     x0 <- c( 3, 1 )
-    
+
     # Lower and upper bounds of control.
     lb <- c( -50, -50 )
     ub <- c(  50,  50 )
-    
+
     # Optimal solution.
     solution.opt <- c( 1, 1 )
-    
+
     # Solve with MMA.
     opts <- list( "algorithm"            = "NLOPT_LD_MMA",
-                  "xtol_rel"             = 1.0e-7, 
+                  "xtol_rel"             = 1.0e-6,
                   "tol_constraints_ineq" = rep( 1.0e-6, 5 ),
                   "print_level"          = 0 )
 
     res <- nloptr(
-                x0          = x0, 
-                eval_f      = eval_f, 
-                lb          = lb, 
-                ub          = ub, 
-                eval_g_ineq = eval_g_ineq, 
+                x0          = x0,
+                eval_f      = eval_f,
+                lb          = lb,
+                ub          = ub,
+                eval_g_ineq = eval_g_ineq,
                 opts        = opts )
-    
+
     # Run some checks on the optimal solution.
-    expect_that( res$solution, equals( solution.opt, tolerance = 1e-7 ) )
+    expect_that( res$solution, equals( solution.opt, tolerance = 1e-6 ) )
     expect_that( all( res$solution >= lb ), is_true() )
     expect_that( all( res$solution <= ub ), is_true() )
-    
+
     # Check whether constraints are violated (up to specified tolerance).
     expect_that( all( eval_g_ineq( res$solution )$constr <= res$options$tol_constraints_ineq ), is_true() )
 } )
