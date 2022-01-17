a74c5baaf3ccc2e23db67cef9444394cab8d3149
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-01-17 21:43:07 +0100

    Revert "Setup rwinlib approach for windows."
    This reverts commit fb9ab322c7f7e7880521e14eb4272e48dc391b6f.

--- tools/winlibs.R
+++
@@ -1,8 +0,0 @@
-# Build against mingw-w64 build of nlopt
-download.file(
-  url = "https://github.com/rwinlib/nlopt/archive/refs/tags/v2.7.1.zip",
-  destfile = "lib.zip",
-  quiet = TRUE
-)
-unzip("lib.zip", exdir = "../src")
-unlink("lib.zip")
