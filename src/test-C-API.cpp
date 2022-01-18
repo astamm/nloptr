4cd3192f74de735dad2510f22044c835e2a218a0
Aymeric Stamm <aymeric.stamm@math.cnrs.fr>
2022-01-18 18:20:22 +0100

    Change threshold again in C API unit tests.

--- src/test-C-API.cpp
+++ src/test-C-API.cpp
@@ -104,7 +104,7 @@ context("Test C API")
 
     // Check return value.
     expect_true(res.size() == 2);
-    expect_true(abs(res[0] - 1./ 3) < 1.0e-5);
-    expect_true(abs(res[1] - 8./27) < 1.0e-5);
+    expect_true(abs(res[0] - 1./ 3) < 1.0e-4);
+    expect_true(abs(res[1] - 8./27) < 1.0e-4);
   }
 }
