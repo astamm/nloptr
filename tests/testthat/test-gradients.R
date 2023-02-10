a1ce4738aa6ba1ae21d412404ad42e329e4b9f9d
Avraham Adler <avraham.adler@gmail.com>
2023-02-10 00:50:48 -0500

    Add accuracy test for multivariate gradient of scalar function and multivariate Jacobian of vector function

--- tests/testthat/test-gradients.R
+++ tests/testthat/test-gradients.R
@@ -10,6 +10,8 @@
 # Changelog:
 #
 
+tol <- sqrt(.Machine$double.eps)
+
 fn1 <- function(x) sum(x ^ 2)
 fn2 <- function(x) c(3, 5)
 
@@ -26,6 +28,7 @@ expect_error(nl.jacobian("C", fn1),
 expect_error(nl.jacobian(NULL, fn1),
              "Argument 'x' must be a non-empty numeric vector.", fixed = TRUE)
 
+# Test accuracy of multivariate gradient of scalar function
 x0 <- c(-2, 2, 2, -1, -1)
 fnE <- function(x) exp(x[1] * x[2] * x[3] * x[4] * x[5])
 grE <- function(x) {
@@ -36,4 +39,17 @@ grE <- function(x) {
     x[1] * x[2] * x[3] * x[4]) * exp(prod(x))
 }
 
-expect_equal(grE(x0), nl.grad(x0, fnE), tolerance = sqrt(.Machine$double.eps))
+expect_equal(nl.grad(x0, fnE), grE(x0), tolerance = tol)
+
+# Test accuracy of multivariate Jacobian of vector function
+x0 <- 1:3
+fn1 <- function(x) c(3 * x[1L] ^ 2 * x[2L] * log(x[3L]),
+                     x[3] ^ 3 - 2 * x[1L] * x[2L])
+
+jac1 <- function(x) matrix(c(6 * x[1L] * x[2L] * log(x[3L]),
+                             3 * x[1L] ^ 2 * log(x[3L]),
+                             3 * x[1L] ^ 2 * x[2L] / x[3L],
+                             -2 * x[2L], -2 * x[1L], 3 * x[3L] ^ 2),
+                           nrow = 2L, byrow = TRUE)
+
+expect_equal(nl.jacobian(x0, fn1), jac1(x0), tolerance = tol)
