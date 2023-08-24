a4c3f7c49a374f47b135eb30a708ee7f751ce2dd
Avraham Adler <avraham.adler@gmail.com>
2023-08-23 22:22:46 -0400

    Convert _output to _stdout in test-print.nloptr

--- inst/tinytest/test-print.nloptr.R
+++ inst/tinytest/test-print.nloptr.R
@@ -8,6 +8,7 @@
 # Test code in print.nloptr function that is not tested elsewhere.
 #
 # Changelog:
+#   2023-08-23: Convert _output to _stdout for tinytest
 #
 
 x0 <- c(3, 3)
@@ -17,14 +18,14 @@ fn <- function(x) (x[1] - 1) ^ 2 + (x[2] - 1) ^ 2
 fit <- nloptr(x0, fn,
               opts = list(algorithm = "NLOPT_LN_NELDERMEAD", xtol_rel = 1e-2))
 
-expect_output(print(fit),
+expect_stdout(print(fit),
               "Optimal value of controls: 1.004579 1.003978", fixed = TRUE)
 
-expect_output(print(fit, 1),
+expect_stdout(print(fit, 1),
               "Optimal value of user-defined subset of controls: 1.004579",
               fixed = TRUE)
 
-expect_output(print(fit, 2),
+expect_stdout(print(fit, 2),
               "Optimal value of user-defined subset of controls: 1.003978",
               fixed = TRUE)
 
@@ -32,13 +33,13 @@ expect_output(print(fit, 2),
 fit <- nloptr(x0, fn,
        opts = list(algorithm = "NLOPT_LN_NELDERMEAD", xtol_rel = 1e-8))
 
-expect_output(print(fit),
+expect_stdout(print(fit),
               "Current value of controls: 0.9999994 1.000001", fixed = TRUE)
 
-expect_output(print(fit, 1),
+expect_stdout(print(fit, 1),
               "Current value of user-defined subset of controls: 0.9999994",
               fixed = TRUE)
 
-expect_output(print(fit, 2),
+expect_stdout(print(fit, 2),
               "Current value of user-defined subset of controls: 1.000001",
               fixed = TRUE)
