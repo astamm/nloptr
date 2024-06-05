165b4f7799d961f3183d89b6dd85f977c44c034a
Avraham Adler <avraham.adler@gmail.com>
2024-06-04 22:11:42 -0400

    Call the hinjac parameter by its proper name.

--- R/mma.R
+++ R/mma.R
@@ -147,8 +147,8 @@ mma <- function(x0, fn, gr = NULL, lower = NULL, upper = NULL, hin = NULL,
     if (is.null(hinjac)) {
       hinjac <- function(x) nl.jacobian(x, hin)
     } else if (deprecatedBehavior) {
-      message("The old behavior for hin >= 0 has been deprecated. Please ",
-              "restate the inequality to be <=0. The ability to use the old",
+      message("The old behavior for hinjac >= 0 has been deprecated. Please ",
+              "restate the inequality to be <=0. The ability to use the old ",
               "behavior will be removed in a future release.")
       .hinjac <- match.fun(hinjac)
       hinjac <- function(x) -.hinjac(x)
