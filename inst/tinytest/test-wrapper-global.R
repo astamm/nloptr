61a036565b6f85dc6a198233e665ef51e272a1c8
Avraham Adler <avraham.adler@gmail.com>
2024-06-25 12:37:10 -0400

    Correct the tests when passing hin.

--- inst/tinytest/test-wrapper-global.R
+++ inst/tinytest/test-wrapper-global.R
@@ -185,8 +185,8 @@ isresTest <- isres(x0, rbf, lb, ub, hin = hin, maxeval = 2e4L,
 
 expect_equal(isresTest$par, isresControl$solution, tolerance = 1e-4)
 expect_equal(isresTest$value, isresControl$objective, tolerance = 1e-4)
-expect_identical(stogoTest$convergence, stogoControl$status)
-expect_identical(stogoTest$message, stogoControl$message)
+expect_identical(isresTest$convergence, isresControl$status)
+expect_identical(isresTest$message, isresControl$message)
 
 # Test deprecated message
 expect_warning(isres(x0, rbf, lower = lb, upper = ub, hin = hin2,
