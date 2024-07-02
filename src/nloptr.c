6307736d02232e49dcdce13db18379aa98dc9bae
Avraham Adler <avraham.adler@gmail.com>
2024-07-02 16:28:11 -0400

    All should be > 0.

--- src/nloptr.c
+++ src/nloptr.c
@@ -350,7 +350,7 @@ typedef struct {
   SEXP R_eval_f;
   SEXP R_environment;
   int  num_iterations;
-  int  print_level;
+  unsigned int print_level;
 } func_objective_data;
 
 // Define function that calls user-defined objective function in R
@@ -440,7 +440,7 @@ double func_objective(unsigned n, const double *x, double *grad, void *data) {
 typedef struct {
     SEXP R_eval_g;
     SEXP R_environment;
-    int print_level;
+    unsigned int print_level;
 } func_constraints_ineq_data;
 
 /*
@@ -543,7 +543,7 @@ void func_constraints_ineq(unsigned m, double* constraints, unsigned n, const do
 typedef struct {
     SEXP R_eval_g;
     SEXP R_environment;
-    int print_level;
+    unsigned int print_level;
 } func_constraints_eq_data;
 
 /*
