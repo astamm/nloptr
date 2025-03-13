5be21b0c8a4f176c64da85ba806dddf22a88dd5d
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2025-03-13 11:34:37 +0100

    Throw warning instead of error upon wrong mandatory arguments in nloptr().

--- R/nloptr.R
+++ R/nloptr.R
@@ -323,9 +323,9 @@ nloptr <- function(x0,
                    ...) {
 
   # check arguments
-  if (!is.numeric(x0)) stop("x0 must be numeric\n")
-  if (length(x0) == 0) stop("x0 must have length > 0\n")
-  if (!is.function(eval_f)) stop("eval_f must be a function\n")
+  if (!is.numeric(x0)) warning("x0 must be numeric\n")
+  if (length(x0) == 0) warning("x0 must have length > 0\n")
+  if (!is.function(eval_f)) warning("eval_f must be a function\n")
 
   # internal function to check the arguments of the functions
   .checkfunargs <- function(fun, arglist, funname) {
