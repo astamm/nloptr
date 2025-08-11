90c6b0c2b6d54259586e67fe9dd7dab909431323
Aymeric Stamm <aymeric.stamm@cnrs.fr>
2025-08-11 22:30:07 +0200

    Clean C code.

--- src/nloptr.h
+++ src/nloptr.h
@@ -6,7 +6,7 @@
 #include <R.h>
 #include <Rinternals.h>
 
-#define AS_INTEGER(x)	Rf_coerceVector(x, INTSXP)
+#define AS_INTEGER(x) Rf_coerceVector(x, INTSXP)
 
 // Extracts element with name 'str' from R object 'list' & returns that element.
 SEXP getListElement(SEXP list, char *str);
@@ -36,9 +36,9 @@ double func_objective(unsigned n, const double *x, double *grad, void *data);
 
 // Define structure that contains data to pass to the constraint function.
 typedef struct {
-    SEXP R_eval_g;
-    SEXP R_environment;
-    int print_level;
+  SEXP R_eval_g;
+  SEXP R_environment;
+  int print_level;
 } func_constraints_ineq_data;
 
 /*
@@ -48,16 +48,18 @@ typedef struct {
  * constraints : value of the constraints evaluated at x
  * n           : number of variables
  * x           : point where we want to evaluate the constraints
- * grad        : value of the gradient of the constraints, grad[i*n + j] = \frac{ \partial c_i }{ \partial x_j }
- * data        : additional data that we need to evaluate the function (e.g. the R function)
-*/
-void func_constraints_ineq(unsigned m, double* constraints, unsigned n, const double* x, double* grad, void* data);
+ * grad        : value of the gradient of the constraints, grad[i*n + j] =
+ * \frac{ \partial c_i }{ \partial x_j } data        : additional data that we
+ * need to evaluate the function (e.g. the R function)
+ */
+void func_constraints_ineq(unsigned m, double *constraints, unsigned n,
+                           const double *x, double *grad, void *data);
 
 // Define structure that contains data to pass to the constraint function.
 typedef struct {
-    SEXP R_eval_g;
-    SEXP R_environment;
-    int print_level;
+  SEXP R_eval_g;
+  SEXP R_environment;
+  int print_level;
 } func_constraints_eq_data;
 
 /*
@@ -67,27 +69,47 @@ typedef struct {
  * constraints : value of the constraints evaluated at x
  * n           : number of variables
  * x           : point where we want to evaluate the constraints
- * grad        : value of the gradient of the constraints, grad[i*n + j] = \frac{ \partial c_i }{ \partial x_j }
- * data        : additional data that we need to evaluate the function (e.g. the R function)
-*/
-void func_constraints_eq(unsigned m, double* constraints, unsigned n, const double* x, double* grad, void* data);
+ * grad        : value of the gradient of the constraints, grad[i*n + j] =
+ * \frac{ \partial c_i }{ \partial x_j } data        : additional data that we
+ * need to evaluate the function (e.g. the R function)
+ */
+void func_constraints_eq(unsigned m, double *constraints, unsigned n,
+                         const double *x, double *grad, void *data);
 
-/*
- * double minf_max - stop if the objective function value drops below minf_max. (Set to -HUGE_VAL to ignore.)
- * double ftol_rel,
- * double ftol_abs - relative and absolute tolerances in the objective function value. (Set to zero to ignore.)
- * double xtol_rel, *xtol_abs - relative and absolute tolerances in the optimization parameter values. xtol_abs
- *                              should either be NULL, in which case it is ignored (equivalent to zero tolerance),
- *                              or a single value in which case it is applied to all parameters, or an array of
- *                              length n containing absolute tolerances in each parameter x[i]. Set any tolerance
- *                              value to zero for it to be ignored.
- * double *x_weights - weights on the optimization parameters. Can be a single value in which case it is applied to
- *                     all parameters, or an array of length n containing weights for each of the n parameters.
- *                     Set any weight value to zero for it to be ignored. (Set to NULL to ignore.)
- * int maxeval - stop if the objective function is evaluated at least maxeval times. Set to zero to ignore.
- * double maxtime - stop if the elapsed wall-clock time, in seconds, exceeds maxtime. Set to zero to ignore.
-*/
-nlopt_opt getOptions(SEXP R_options, int num_controls, int *flag_encountered_error);
+/**
+ * @brief Get options from R options list
+ *
+ * @details The R options list can contain the following options:
+ * - minf_max: stop if the objective function value drops below minf_max. (Set
+ * to -HUGE_VAL to ignore.)
+ * - ftol_rel, ftol_abs: relative and absolute tolerances in the objective
+ * function value. (Set to zero to ignore.)
+ * - xtol_rel, xtol_abs: relative and absolute tolerances in the optimization
+ * parameter values. xtol_abs should either be NULL, in which case it is ignored
+ * (equivalent to zero tolerance), or a single value in which case it is applied
+ * to all parameters, or an array of length n containing absolute tolerances in
+ * each parameter x[i]. Set any tolerance value to zero for it to be ignored.
+ * - x_weights: weights on the optimization parameters. Can be a single value in
+ * which case it is applied to all parameters, or an array of length n
+ * containing weights in each parameter x[i]. Set any weight value to zero for
+ * it to be ignored.
+ * - maxeval: stop if the objective function is evaluated at least maxeval
+ * times. Set to zero to ignore.
+ * - maxtime: stop if the elapsed wall clock time, in seconds, exceeds
+ * maxtime. Set to zero to ignore.
+ * - tol_constraints_ineq
+ * - tol_constraints_eq
+ *
+ * @param R_options R options list
+ * @param num_controls number of controls
+ * @param flag_encountered_error flag to check if error has been encountered
+ *
+ * @return nlopt_opt
+ *
+ * @author: Jelmer Ypma
+ */
+nlopt_opt getOptions(SEXP R_options, int num_controls,
+                     int *flag_encountered_error);
 
 SEXP convertStatusToMessage(nlopt_result status);
 
